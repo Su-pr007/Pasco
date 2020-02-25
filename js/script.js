aa4397f73f4b292ac715f7042da1986c94c87f40
Su-pr007 <maksimka.suvorov.01@list.ru>
2020-02-25 17:02:53 +0300

    24022020

--- js/script.js
+++ js/script.js
@@ -1,21 +1,10 @@
 'use strict'
 $(document).ready(init);
 function init(){
-	$('.lang-block').click(()=>{
-		SwitchVisibleOf('.appearable-window');
-		if($('.language-text img')[0].style.transform!='rotateX(180deg)'){
-			$('.language-text img')[0].style.transform='rotateX(180deg)';
-		}
-		else{
-			$('.language-text img')[0].style.transform='rotateX(0deg)'
-		}
-	})
+
 }
-function SwitchVisibleOf(arg){
-	if($(arg)[0].style.display=='none'){
-		$(arg)[0].style.display='block';
-	}
-	else{
-		$(arg)[0].style.display='none';
-	}
+function changeSelect(path, block, selTo){
+	console.log(path+' '+selTo);
+	$(path+' '+block).removeClass('selected');
+	$(path+' '+selTo).addClass('selected');
 }
