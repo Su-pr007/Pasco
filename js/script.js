d7693d350636e534960e98973aeca56261f745c3
Su-pr007 <maksimka.suvorov.01@list.ru>
2020-03-07 08:37:33 +0300

    06032020

--- js/script.js
+++ js/script.js
@@ -1,55 +1,79 @@
 'use strict'
+
+
 $(document).ready(init);
 function init(){
-	$('.type-3').last().addClass('m-r0');
-	$('.type-4').last().addClass('m-r0');
-	$('.main-footer .photos').height($('.main-footer .photos img').height());
-	$('.subscribe-field .mail, .subscribe-field .blue-button').height($('.subscribe-field>span').height()-34);
-	$('.subscribe-field .mail').width($('.subscribe-field').width()-$('.subscribe-field .blue-button').width()-$('.subscribe-field>span').width()-150);
-	$('.type-2 .blog-bottom').width($('.type-2').width()-$('.type-2 .blog-top').width());
-	$('.type-3 .blog-top').height($('.type-3').height()-$('.type-3 .blog-bottom').height());
-	$('.right-panel_bg').width($('body').width()-$('.right-panel').offset().left+40);
-	$('.type-1 .blog-top').height($('.type-1 .blog-top img').height());
-	$('.type-4 .blog-top').height($('.type-4 .blog-top img').height());
-	$('.content .image').height($('.content .image').height()+20);
-	$('.content .image').width($('.content .image').height());
-	$('.main-footer .twitter .twitter-message').width($('.main-footer .twitter .twitter-message').width()-$('.main-footer .twitter-icon').width()-20);
-	$('.widget.flickr .img-container').height($('.widget.flickr .img-container').width());
-	for(let i=1; i<=$('.widget.flickr .img-container').length; i++){
-		if($('.widget.flickr .img-container:nth-child('+i+') img').height()>$('.widget.flickr .img-container:nth-child('+i+') img').width()){
-			$('.widget.flickr .img-container:nth-child('+i+') img').width($('.widget.flickr .img-container:nth-child('+i+')').width());
+	if($(window).width()>920){
+
+		let blogs=$('.blog');
+		for(let i = 0; i<blogs.length; i++){
+			switch(i%9){
+				case 0:
+					blogs[i].classList.add('type-1');
+					break;
+				case 1:
+				case 2:
+				case 3:
+					blogs[i].classList.add('type-2');
+					break;
+				case 4:
+				case 5:
+				case 6:
+					blogs[i].classList.add('type-3');
+					break;
+				case 7:
+				case 8:
+					blogs[i].classList.add('type-4');
+					break;
+				default:
+					continue;
+			}
+		}
+		$('.type-3').last().addClass('m-r0');
+		$('.type-4').last().addClass('m-r0');
+		$('.main-footer .photos').height($('.main-footer .photos img').height());
+		$('.subscribe-field .mail, .subscribe-field .blue-button').height($('.subscribe-field>span').height()-34);
+		$('.subscribe-field .mail').width($('.subscribe-field').width()-$('.subscribe-field .blue-button').width()-$('.subscribe-field>span').width()-150);
+		$('.type-2 .blog-top').width($('.type-2 .blog-top').first().width())
+		$('.type-2 .blog-bottom').width($('.type-2').width()-$('.type-2 .blog-top').width());
+		$('.right-panel_bg').width($('body').width()-$('.right-panel').offset().left+40);
+		let a=[];
+		$('.blogs .type-3 .blog-top').toArray().forEach((e)=>{  
+			a.push(e["offsetHeight"]);
+		});
+		$('.blogs .type-3 .blog-top').height(a.sort().shift());
+		a=undefined;
+		$('.type-4 .blog-top').height($('.type-4 .blog-top img').height());
+		resizeImg('.widget.flickr .img-container');
+		$('.main-footer .twitter .twitter-message').width($('.main-footer .twitter .twitter-message').width()-$('.main-footer .twitter-icon').width()-20);
+		$('.widget.flickr .img-container').height($('.widget.flickr .img-container').width());
+		return true
+	}
+	else{
+		
+	}
+}
+
+function resizeImg(imgContainer){
+	for(let i=0; i<$(imgContainer).length; i++){
+		if($(imgContainer+':eq('+i+') img').height()>$(imgContainer+':eq('+i+') img').width()){
+			$(imgContainer+':eq('+i+') img').width('100%');
 		}
 		else{
-			$('.widget.flickr .img-container:nth-child('+i+') img').height($('.widget.flickr .img-container:nth-child('+i+')').height())
+			$(imgContainer+':eq('+i+') img').height('100%')
 		}
 	}
-	$('.main-footer .news form').on('submit', (e)=>{
-		e.preventDefault();
-	})
 }
+
+// Смена выбранного элемента
 function changeSelect(path, block, selTo){
 	$(path+' '+block).removeClass('selected');
 	$(path+' '+selTo).addClass('selected');
 }
-// function openPagesMenu(number){
-// 	if($('.menu>li:nth-child('+number+') .inner_menu').css('display')=='block'){
-// 		$('.menu>li .padding').css('borderBottom', 'none');
-// 		$('.menu>li .inner_menu').hide();
-// 		$('.menu>li svg line').show();
-// 	}
-// 	else{
-// 		$('.pages .menu>li .inner_menu').hide();
-// 		$('.menu>li .padding').css('borderBottom', 'none');
-// 		$('.menu>li:nth-child('+number+') .padding').css('borderBottom', '1px solid #999');
-// 		$('.menu>li:nth-child('+number+') .inner_menu').show();
-// 		$('.menu>li svg line').show();
-// 		$('.menu>li:nth-child('+number+') svg line:nth-child(2)').hide();
-// 	}
-// }
-let asd1;
+// открытие / закрытие навигационного меню в footer
 $('.menu>li').click((qq)=>{
 	let number=qq['currentTarget'].attributes['name']['nodeValue'];
-	if($('.menu>li:nth-child('+number+') .inner_menu').css('display')=='block'){
+	if($('.menu>li:eq('+number+') .inner_menu').css('display')=='block'){
 		$('.menu>li .padding').css('borderBottom', 'none');
 		$('.menu>li .inner_menu').hide();
 		$('.menu>li svg line').show();
@@ -57,9 +81,12 @@ $('.menu>li').click((qq)=>{
 	else{
 		$('.pages .menu>li .inner_menu').hide();
 		$('.menu>li .padding').css('borderBottom', 'none');
-		$('.menu>li:nth-child('+number+') .padding').css('borderBottom', '1px solid #999');
-		$('.menu>li:nth-child('+number+') .inner_menu').show();
+		$('.menu>li:eq('+number+') .padding').css('borderBottom', '1px solid #999');
+		$('.menu>li:eq('+number+') .inner_menu').show();
 		$('.menu>li svg line').show();
-		$('.menu>li:nth-child('+number+') svg line:nth-child(2)').hide();
+		$('.menu>li:eq('+number+') svg line:nth-child(2)').hide();
 	}
 });
+window.addEventListener('resize', ()=>{
+	setTimeout(init, 400)
+}, false);
